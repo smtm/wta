8a4bf18a339d8cc87fb61714931cf7b37622b3aa
Roland Oth <roland.oth@gmail.com>
2013-01-13 13:00:51 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nextout::Application.config.secret_token = '76b683fc03cf11d433873e59b69bc7cf4424f9fb5624940166639cc355b059fda03fd87b18c8e37bf6c4effc5c3407ba7f5fb020ab911abe20f934f22ef288fe'
